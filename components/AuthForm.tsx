a689fb0505975979bdeb4bf1a8954a4538c82134
Darnel Castor <darnelcastor2@gmail.com>
2025-03-26 11:18:29 -0400

    add new components for form handling and UI; update layout and package dependencies

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,9 +1,48 @@
-import react from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+import React from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import FormField from "./FormField";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
 
 const AuthForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
-    <div>
-      <h1>AuthForm</h1>
+    <div className="card-border lg:min-w-[566px]">
+      <div className="flex flex-col gap-6 card py-14 px-10">
+        <div className="flex flex-row gap-2 justify-center">
+          <Image src="/logo.svg" alt="logo" width={38} height={32} />
+          <h2 className="text-primary-100">PrepWise</h2>
+        </div>
+        <h3>Practice job interviews with AI</h3>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-6 mt-4 form"
+          >
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 };
