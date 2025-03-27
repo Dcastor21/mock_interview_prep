8930b86028843792bf2e14e2a5b98e0b64a45eed
Darnel Castor <darnelcastor2@gmail.com>
2025-03-27 12:57:25 -0400

    refactor(AuthForm): improve user feedback and update form field placeholders for clarity

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,27 +1,29 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
-import React from "react";
-import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import Image from "next/image";
 import Link from "next/link";
 import { toast } from "sonner";
-import FormField from "./FormField";
+import FormField from "@/components/FormField";
 import { useRouter } from "next/navigation";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
     name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
     email: z.string().email(),
-    password: z.string().min(6),
+    password: z.string().min(3),
   });
 };
 
 const AuthForm = ({ type }: { type: FormType }) => {
   const router = useRouter();
   const formSchema = authFormSchema(type);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -30,30 +32,33 @@ const AuthForm = ({ type }: { type: FormType }) => {
       password: "",
     },
   });
+
   function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-up") {
-        console.log(values);
-        toast.success("Account created successfully");
+        toast.success("Account created successfully. Please sign in.");
         router.push("/sign-in");
       } else {
-        toast.success("Sign In successfully");
-        router.push("/sign-in");
+        toast.success("Sign in successfully.");
+        router.push("/");
       }
     } catch (error) {
       console.log(error);
-      toast.error(`${error}`);
+      toast.error(`There was an error: ${error}`);
     }
   }
+
   const isSignIn = type === "sign-in";
+
   return (
     <div className="card-border lg:min-w-[566px]">
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
-          <Image src="/logo.svg" alt="logo" width={38} height={32} />
+          <Image src="/logo.svg" alt="logo" height={32} width={38} />
           <h2 className="text-primary-100">PrepWise</h2>
         </div>
-        <h3>Practice job interviews with AI</h3>
+
+        <h3>Practice job interview with AI</h3>
 
         <Form {...form}>
           <form
@@ -72,33 +77,35 @@ const AuthForm = ({ type }: { type: FormType }) => {
               control={form.control}
               name="email"
               label="Email"
-              placeholder="Your Email"
+              placeholder="Your email address"
               type="email"
             />
+
             <FormField
               control={form.control}
               name="password"
               label="Password"
-              placeholder="Your Password"
+              placeholder="Enter your password"
               type="password"
             />
+
             <Button className="btn" type="submit">
-              {isSignIn ? "Sign In" : "Create Account"}
+              {isSignIn ? "Sign in" : "Create an Account"}
             </Button>
           </form>
         </Form>
+
         <p className="text-center">
-          {isSignIn ? "Don't have an account?" : "Already have an account?"}
+          {isSignIn ? "No account yet?" : "Have an account already?"}
           <Link
             href={!isSignIn ? "/sign-in" : "/sign-up"}
             className="font-bold text-user-primary ml-1"
           >
-            {isSignIn ? "Sign in" : "Sign Up"}
+            {!isSignIn ? "Sign in" : "Sign up"}
           </Link>
         </p>
       </div>
     </div>
   );
 };
-
 export default AuthForm;
