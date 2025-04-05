2187a4e1046a732827c52704a5ce1a4ac05b980e
Darnel Castor <darnelcastor2@gmail.com>
2025-04-05 18:43:09 -0400

    added interview page

--- lib/actions/auth.actions.ts
+++ lib/actions/auth.actions.ts
@@ -3,58 +3,68 @@
 import { auth, db } from "@/firebase/admin";
 import { cookies } from "next/headers";
 
-const ONE_WEEK = 60 * 60 * 24 * 7;
+// Session duration (1 week)
+const SESSION_DURATION = 60 * 60 * 24 * 7;
+
+// Set session cookie
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+
+  // Create session cookie
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: SESSION_DURATION * 1000, // milliseconds
+  });
+
+  // Set cookie in the browser
+  cookieStore.set("session", sessionCookie, {
+    maxAge: SESSION_DURATION,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
+}
+
 export async function signUp(params: SignUpParams) {
   const { uid, name, email } = params;
 
   try {
+    // check if user exists in db
     const userRecord = await db.collection("users").doc(uid).get();
-
-    if (userRecord.exists) {
+    if (userRecord.exists)
       return {
         success: false,
-        error: "User already exists",
+        message: "User already exists. Please sign in.",
       };
-    }
 
+    // save user to db
     await db.collection("users").doc(uid).set({
       name,
       email,
+      // profileURL,
+      // resumeURL,
     });
+
     return {
       success: true,
       message: "Account created successfully. Please sign in.",
     };
-  } catch (e: any) {
-    console.error("Error Creating User", e);
+  } catch (error: any) {
+    console.error("Error creating user:", error);
 
-    if (e.code === "auth/email-already-in-use") {
+    // Handle Firebase specific errors
+    if (error.code === "auth/email-already-exists") {
       return {
         success: false,
-        error: "Email already in use.",
+        message: "This email is already in use",
       };
     }
 
     return {
       success: false,
-      error: "Failed to create an account",
+      message: "Failed to create account. Please try again.",
     };
   }
-}
-
-export async function setSessionCookie(idToken: string) {
-  const cookieStore = await cookies();
-
-  const sessionCookie = await auth.createSessionCookie(idToken, {
-    expiresIn: ONE_WEEK * 1000,
-  });
-  cookieStore.set("session", sessionCookie, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    maxAge: ONE_WEEK,
-    path: "/",
-    sameSite: "lax",
-  });
 }
 
 export async function signIn(params: SignInParams) {
@@ -69,8 +79,8 @@ export async function signIn(params: SignInParams) {
       };
 
     await setSessionCookie(idToken);
-  } catch (e: any) {
-    console.error(e);
+  } catch (error: any) {
+    console.log("");
 
     return {
       success: false,
