935ebca4ce9243e121f563ebc80cf74cc32e6f4f
Darnel Castor <darnelcastor2@gmail.com>
2025-04-05 22:44:52 -0400

    feat(agent): add message display functionality to Agent component

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -12,6 +12,8 @@ enum CallStatus {
 const Agent = ({ userName }: AgentProps) => {
   const callStatus = CallStatus.FINISHED;
   const isSpeaking = true;
+  const message = ["how are you", "i am good"];
+  const lastMessage = message[message.length - 1];
   return (
     <>
       <div className="call-view">
@@ -41,6 +43,21 @@ const Agent = ({ userName }: AgentProps) => {
           </div>
         </div>
       </div>
+      {message.length > 0 && (
+        <div className="transcript-border">
+          <div className="transcript">
+            <p
+              key={lastMessage}
+              className={cn(
+                "transiton-opacity duration-500 opacity-0",
+                "animate-fadeIn opacity-100"
+              )}
+            >
+              {lastMessage}
+            </p>
+          </div>
+        </div>
+      )}
       <div className=" w-full flex justify-center">
         {CallStatus !== "ACTIVE" ? (
           <button className="relative btn-call">
